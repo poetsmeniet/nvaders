52d50fb4a5dd16bdaf5aeb4655b38fdc2282b906
thomas <thomas@geenbs.nl>
2017-01-23 22:34:33 +0100

    added enemy movement

--- main.c
+++ main.c
@@ -2,52 +2,58 @@
 #include <unistd.h>
 
 #define DELAY1 10000
-#define DELAY2 30000
+#define DELAY2 60000
 #define MVSTEP 0.1
 
-struct fighter{
+struct f{
     int x;
     int y;
 };
-struct enemy{
+struct e{
     int x;
     int y;
 };
 
-void setStage(struct fighter *ship1, struct enemy *enemy, int max_x, int max_y);
-void printObjects(struct fighter * ship1, struct enemy *enemy, int max_x, int max_y);
-void moveShip(struct fighter *ship, int mv);
-void moveEnemy(struct enemy *enemy);
+void setStage(struct f *ship, struct e *enemy, int max_x, int max_y);
+void printObjects(struct f * ship, struct e *enemy, int max_x, int max_y);
+void moveShip(struct f *ship, int mv);
+void moveEnemy(struct e *enemy, int max_x, int max_y, int d);
 
 int main(int argc, char *argv[]) {
     initscr();
     noecho();
     keypad(stdscr, TRUE);
     curs_set(FALSE);
+    timeout(0); /* set delay non blocking */
 
     int max_y = 0; int max_x = 0;
-    int mv;
+    int mv, d;
 
-    struct fighter ship1;
-    ship1.x=0;
-    ship1.y=0;
+    struct f ship;
+    ship.x=0;
+    ship.y=0;
 
-    struct enemy enemy;
+    struct e enemy;
     enemy.x=0;
     enemy.y=0;
    
     // Global var `stdscr` is created by the call to `initscr()`
     getmaxyx(stdscr, max_y, max_x);
     getmaxyx(stdscr, max_y, max_x);//move to on winch
-    setStage(&ship1, &enemy, max_x, max_y);//move to on winch
+    setStage(&ship, &enemy, max_x, max_y);//move to on winch
    
     while(1) {
         mv=getch();
 
-        moveShip(&ship1, mv);
-        moveEnemy(&enemy);
+        moveShip(&ship, mv);
 
-        printObjects(&ship1, &enemy, max_x, max_y);
+        if(enemy.x == (max_x - 10))
+            d=1;
+        if(enemy.x == 0)
+            d=0;
+        moveEnemy(&enemy, max_x, max_y, d);
+
+        printObjects(&ship, &enemy, max_x, max_y);
    
         usleep(DELAY2);
     }
@@ -55,39 +61,43 @@ int main(int argc, char *argv[]) {
     endwin();
 }
 
-void moveShip(struct fighter *ship1, int mv){
+void moveShip(struct f *ship, int mv){
         if(mv == KEY_LEFT){
-            ship1->x--;
+            ship->x--;
         }else if(mv == KEY_RIGHT){
-            ship1->x++;
+            ship->x++;
         }else{
-            //ship1.x=x;
+            //ship.x=x;
         }
 }
 
-void moveEnemy(struct enemy *enemy){
-    enemy->x++;
+void moveEnemy(struct e *enemy, int max_x, int max_y, int d){
+    if(d)
+        enemy->x--;
+    else
+        enemy->x++;
 }
-void printObjects(struct fighter * ship1, struct enemy *enemy, int max_x, int max_y){
+
+void printObjects(struct f * ship, struct e *enemy, int max_x, int max_y){
     clear();
-    mvprintw(enemy->y, enemy->x, "^^^^^^^^^^^^^^^");
-    mvprintw(max_y-1, ship1->x, "[{O}]");
+    mvprintw(enemy->y, enemy->x, "0000000000");
+    mvprintw(max_y-1, ship->x, "[{O}]");
     refresh();
 }
 
-void setStage(struct fighter *ship1, struct enemy *enemy, int max_x, int max_y){
+void setStage(struct f *ship, struct e *enemy, int max_x, int max_y){
     while(1) {
         enemy->x++;
 
-        if(ship1->x == (max_x / 2)){
+        if(ship->x == (max_x / 2)){
             break;
         }
-        if(ship1->x >= (max_x / 2))
-            ship1->x--;
+        if(ship->x >= (max_x / 2))
+            ship->x--;
         else
-            ship1->x++;
+            ship->x++;
         
-        printObjects(ship1, enemy, max_x, max_y);
+        printObjects(ship, enemy, max_x, max_y);
 
         usleep(DELAY1);
     }
